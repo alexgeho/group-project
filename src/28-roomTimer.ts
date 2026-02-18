6b2dc817d805ade98fe76266809e5e6f4922e7b3
Charoua <xarakalpa@gmail.com>
2026-02-18 14:18:17 +0100

    fix: adjust room timer

--- src/28-roomTimer.ts
+++ src/28-roomTimer.ts
@@ -1,24 +1,27 @@
 let seconds = 0
 let intervalId: number | null = null 
 
-export function startRoomTimer() {
+export function startRoomTimer(updateUI: (time: number) => void) {
     if (intervalId !== null) {
         return
     }
         seconds = 0
 
         intervalId = setInterval(() => {
-            seconds++
+            seconds++;
+            updateUI(seconds);
         }, 1000)
     }
 
-    export function stopRoomTimer() {
+    export function stopRoomTimer(): number {
         if (intervalId === null) {
             return 0
         }
 
         clearInterval(intervalId)
         intervalId = null
+
+        console.log("Room time", seconds)
 
         return seconds
     }
