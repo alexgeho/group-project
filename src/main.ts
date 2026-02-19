39ae531d4050d6de76eea91bd590e85089b7d684
Mojtaba <mojtaba.mobasheri@hotmail.com>
2026-02-19 05:46:17 +0100

    update-made basic html better.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import './styles/style.scss'
 
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('main > section');
+  sections.forEach(section => section.classList.add('hidden'));
 
+  // Only show welcome page to begin with.
+  document.querySelector('.welcome-page')?.classList.remove('hidden');
+});
 
