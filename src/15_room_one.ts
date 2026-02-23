5e54221f7c495054c1764b75663f012faa5ade7b
Angelie <angelie.angman@gmail.com>
2026-02-23 16:14:45 +0100

    refactor: reorganize room-one structure

--- src/15_room_one.ts
+++ src/15_room_one.ts
@@ -1,13 +1,27 @@
-const memoryContainer = document.getElementById("firewall");
+/**
+ * FIREWALL - Memory Game
+ *
+ * The player have to get pairs of cards with the same emoji.
+ * The cards are placed face down and the player can flip two cards at a time.
+ * If the cards match, they stay face up.
+ * If they don't match, they are flipped back face down.
+ * The player wins when all pairs of cards are matched.
+ */
 
-export function loadRoomOne(onBack: () => void): void {
-  if (!memoryContainer) return;
+// Skickar vidare parametern från backButtonSetup()
+export function loadRoomOne(onBack: () => void) {
+  backButtonSetup(onBack);
+}
 
-  memoryContainer.innerHTML = `
-    <p>Welcome to Room One Firewall</p>
-    <button id="backToRooms">Back</button>
-  `;
+function backButtonSetup(onBack: () => void) {
+  const backButton = document.querySelector("back");
 
-  const backButton = document.getElementById("backToRooms");
   backButton?.addEventListener("click", onBack);
 }
+
+const gameBoard = document.querySelector("#firewall");
+
+gameBoard!.innerHTML = `
+    <p>Welcome to Room One Firewall :)</p>
+    <button id="backToRooms">Back</button>
+  `;
