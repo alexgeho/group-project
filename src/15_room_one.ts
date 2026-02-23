2da26c68f5063cbdc3341a502ae39802ff15aadc
Angelie <angelie.angman@gmail.com>
2026-02-23 17:30:47 +0100

    refactor: reorganize structure

--- src/15_room_one.ts
+++ src/15_room_one.ts
@@ -8,25 +8,42 @@
  * The player wins when all pairs of cards are matched.
  */
 
-// goToRoom anropar loadRoomOne
-// tar emot parametern
-// skickar vidare parametern
-export function loadRoomOne(onBack: () => void) {
-  backButtonSetup(onBack);
-}
+// 1. DATA - the cards for the memory game
+// 2. VIEW - HTML markup
+// 3. RENDER - inject the wiew into doom
+// 4. EVENTS
+// 5. GAME LOGIC
 
-// Här sätts event-lyssnare
-function backButtonSetup(onBack: () => void) {
-  const backButton = document.querySelector("#back");
+// 1. Data
+const memoryCards = [
+  { emoji: "🍎", matched: false },
+  { emoji: "🍎", matched: false },
+];
 
-  // klick på back button triggar backToLobby funktionen
-  backButton?.addEventListener("click", onBack);
-}
+memoryCards;
 
-// View
+// 2. View
 const gameBoard = document.querySelector("#firewall");
 
 gameBoard!.innerHTML = `
     <p>Welcome to Room One Firewall :)</p>
     <button id="back">Back</button>
   `;
+
+// 3. Render
+export function loadRoomOne(onBack: () => void) {
+  backButtonSetup(onBack);
+}
+
+// 4. Här sätts event-lyssnare
+function backButtonSetup(onBack: () => void) {
+  const backButton = document.querySelector("#back");
+
+  backButton?.addEventListener("click", onBack);
+}
+
+// 5. Logic
+// false -> not matched
+// true -> matched
+// if the card is false -> turn
+// if the card is true -> stay
