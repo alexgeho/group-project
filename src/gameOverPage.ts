afd5ec128879fdd0400298197d56260f38316713
Mojtaba <mojtaba.mobasheri@hotmail.com>
2026-02-24 09:54:25 +0100

    feat: implement bug-fixing game mechanics in Room Five and add game over page

---
+++ src/gameOverPage.ts
@@ -0,0 +1,21 @@
+import { goToLobby } from "./gotoLobby";
+
+export function loadGameOverPage(message: string, isWin: boolean): void {
+
+  const gameOverContainer = document.querySelector(".game-over");
+  if (!gameOverContainer) return;
+
+  document.querySelectorAll('main > section').forEach(section => section.classList.add('hidden'));
+  gameOverContainer.classList.remove('hidden');
+
+  gameOverContainer.innerHTML = `
+    ${isWin ? `<h2 class="win">Congratulations!</h2>        <p class="p-message-win">${message}</p>` : `<h2 class="lose">Game Over</h2> <p class="p-message-lose">${message}</p>`}
+    
+    <button id="backToLobbyBtn" class="btn-primary">Back to lobby</button>
+  `;
+
+  const backButton = document.getElementById("backToLobbyBtn");
+  backButton?.addEventListener("click", () => {
+    goToLobby();
+  });
+}
--- src/login2.ts
+++ src/login2.ts
@@ -12,9 +12,9 @@ export function loginUser(e: Event): void {
     id: crypto.randomUUID(),
     name: usernameInput.value.trim(),
     points: 0,
-    artifacts: ['a', 'b', 'c'],
-    roomTimes: [{ roomId: 2, time: 120 }, { roomId: 4, time: 150 }, { roomId: 3, time: 200 }],
-    roomsCompleted: [1,2,3],
+    artifacts: [],
+    roomTimes: [{ roomId: 3, time: 200 }],
+    roomsCompleted: [],
   };
 
   localStorage.setItem("player", JSON.stringify(player));
