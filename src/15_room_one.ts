be382769d30e9daca32a05fcfd63f856e95def79
Angelie <angelie.angman@gmail.com>
2026-02-24 16:30:15 +0100

    refactor(room-one): remove id and render cards from array

--- src/15_room_one.ts
+++ src/15_room_one.ts
@@ -18,39 +18,34 @@
 // 1. Data
 
 type MemoryCard = {
-  id: number;
   emoji: string;
   flipped: boolean; // är kortet vänt (true)
   matched: boolean; // är kortet matchat (true)
 };
 
-const memoryCards = [
-  { id: 1, emoji: "👻", flipped: false, matched: false },
-  { id: 2, emoji: "👻", flipped: false, matched: false },
-  { id: 3, emoji: "🧠", flipped: false, matched: false },
-  { id: 4, emoji: "🧠", flipped: false, matched: false },
-  { id: 5, emoji: "🌛", flipped: false, matched: false },
-  { id: 6, emoji: "🌛", flipped: false, matched: false },
-  { id: 7, emoji: "🪼", flipped: false, matched: false },
-  { id: 8, emoji: "🪼", flipped: false, matched: false },
-  { id: 9, emoji: "💄", flipped: false, matched: false },
-  { id: 10, emoji: "💄", flipped: false, matched: false },
-  { id: 11, emoji: "🦄", flipped: false, matched: false },
-  { id: 12, emoji: "🦄", flipped: false, matched: false },
+const memoryCards: MemoryCard[] = [
+  { emoji: "👻", flipped: false, matched: false },
+  { emoji: "👻", flipped: false, matched: false },
+  { emoji: "🧠", flipped: false, matched: false },
+  { emoji: "🧠", flipped: false, matched: false },
+  { emoji: "🌛", flipped: false, matched: false },
+  { emoji: "🌛", flipped: false, matched: false },
+  { emoji: "🪼", flipped: false, matched: false },
+  { emoji: "🪼", flipped: false, matched: false },
+  { emoji: "💄", flipped: false, matched: false },
+  { emoji: "💄", flipped: false, matched: false },
+  { emoji: "🦄", flipped: false, matched: false },
+  { emoji: "🦄", flipped: false, matched: false },
 ];
 
-memoryCards;
-
 // 2. state
-// null at default
-let firstSelectedCard: number | null = null;
-let secondSelectedCard: number | null = null;
 
 // 3. View
 function fireWallView() {
   const firewallMarkup = document.querySelector("#firewall");
+  if (!firewallMarkup) return;
 
-  firewallMarkup!.innerHTML = `
+  firewallMarkup.innerHTML = `
     <h2>You've entered a Firewall</h2>
     <p>It is your mission to break it</p>
     <div id="cardBoard" class="card-board"></div>
@@ -61,9 +56,10 @@ function fireWallView() {
 
   let cardsMarkup = "";
 
-  for (let i = 0; i < 12; i++) {
+  // data-id - för att
+  for (let i = 0; i < memoryCards.length; i++) {
     cardsMarkup += `
-  <button id="card" class="card-style">🔥</button>
+  <button class="card" data-id="${i}">${memoryCards[i].emoji}</button>
   `;
   }
 
@@ -76,13 +72,6 @@ export function loadRoomOne(onBack: () => void) {
   backButtonSetup(onBack); // 2️⃣ koppla event
 }
 
-// 4. Här sätts event-lyssnare
-function backButtonSetup(onBack: () => void) {
-  const backButton = document.querySelector("#back");
-
-  backButton?.addEventListener("click", onBack);
-}
-
 // 5. Logic
 
 // 1. tryck på kort 1 -> kortet vänds
@@ -90,3 +79,10 @@ function backButtonSetup(onBack: () => void) {
 // 3. matchar korten?
 // 4. om ja -> gör ingenting
 // 5. om nej -> vänd tillbaka
+
+// 4. Här sätts event-lyssnare
+function backButtonSetup(onBack: () => void) {
+  const backButton = document.querySelector("#back");
+
+  backButton?.addEventListener("click", onBack);
+}
