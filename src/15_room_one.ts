e3fd7e6ffcb8883a31ed21ef2ddbe2970e0c39e0
Angelie <angelie.angman@gmail.com>
2026-02-27 14:29:44 +0100

    refactor(room one): clean up

--- src/15_room_one.ts
+++ src/15_room_one.ts
@@ -8,214 +8,142 @@
  * The player wins when all pairs of cards are matched.
  */
 
-import { stopRoomTimer } from "./28-roomTimer";
-import { showPlayerStats } from "./showPlayerStats";
-
-// ──────────────────────────────────────────────────────────────────────────────────────
-// 🔥 LOAD ROOM ONE
-// ──────────────────────────────────────────────────────────────────────────────────────
-
+// receive a callback (onBack = goToLobby) from outside
 export function loadRoomOne(onBack: () => void) {
-  // 1️⃣ skapa HTML
-  renderRoom();
-  renderCards();
-  // 2️⃣ koppla event
-  backButtonSetup(onBack);
-}
+  interface MemoryCard {
+    emoji: string;
+    flipped: boolean;
+    matched: boolean;
+  }
 
-// ──────────────────────────────────────────────────────────────────────────────────────
-// 🗂 DATA - memory card information
-// ──────────────────────────────────────────────────────────────────────────────────────
+  const memoryCards: MemoryCard[] = [
+    { emoji: "🪐", flipped: false, matched: false },
+    { emoji: "🌙", flipped: false, matched: false },
+    { emoji: "🧠", flipped: false, matched: false },
+    { emoji: "👾", flipped: false, matched: false },
+    { emoji: "🛸", flipped: false, matched: false },
+    { emoji: "🌙", flipped: false, matched: false },
+    { emoji: "🛸", flipped: false, matched: false },
+    { emoji: "🌎", flipped: false, matched: false },
+    { emoji: "🧩", flipped: false, matched: false },
+    { emoji: "🌕", flipped: false, matched: false },
+    { emoji: "🌎", flipped: false, matched: false },
+    { emoji: "🧠", flipped: false, matched: false },
+    { emoji: "🦋", flipped: false, matched: false },
+    { emoji: "🪐", flipped: false, matched: false },
+    { emoji: "🦄", flipped: false, matched: false },
+    { emoji: "🧩", flipped: false, matched: false },
+    { emoji: "👾", flipped: false, matched: false },
+    { emoji: "🦋", flipped: false, matched: false },
+    { emoji: "🌕", flipped: false, matched: false },
+    { emoji: "🦄", flipped: false, matched: false },
+  ];
 
-interface MemoryCard {
-  emoji: string;
-  flipped: boolean; // är kortet vänt (true)
-  matched: boolean; // är kortet matchat (true)
-}
+  // 🖼 View - HTML Markup
+  // ──────────────────────────────────────────────────────────────────────────────────────
 
-const memoryCards: MemoryCard[] = [
-  { emoji: "🪐", flipped: false, matched: false },
-  { emoji: "🌙", flipped: false, matched: false },
-  { emoji: "🧠", flipped: false, matched: false },
-  { emoji: "👾", flipped: false, matched: false },
-  { emoji: "🛸", flipped: false, matched: false },
-  { emoji: "🌙", flipped: false, matched: false },
-  { emoji: "🛸", flipped: false, matched: false },
-  { emoji: "🌎", flipped: false, matched: false },
-  { emoji: "🧩", flipped: false, matched: false },
-  { emoji: "🌕", flipped: false, matched: false },
-  { emoji: "🌎", flipped: false, matched: false },
-  { emoji: "🧠", flipped: false, matched: false },
-  { emoji: "🦋", flipped: false, matched: false },
-  { emoji: "🪐", flipped: false, matched: false },
-  { emoji: "🦄", flipped: false, matched: false },
-  { emoji: "🧩", flipped: false, matched: false },
-  { emoji: "👾", flipped: false, matched: false },
-  { emoji: "🦋", flipped: false, matched: false },
-  { emoji: "🌕", flipped: false, matched: false },
-  { emoji: "🦄", flipped: false, matched: false },
-];
+  function renderRoom() {
+    const firewallMarkup = document.querySelector("#firewall");
+    if (!firewallMarkup) return;
 
-// ──────────────────────────────────────────────────────────────────────────────────────
-// 🖼 View - HTML Markup
-// ──────────────────────────────────────────────────────────────────────────────────────
-
-function renderRoom() {
-  const firewallMarkup = document.querySelector("#firewall");
-  if (!firewallMarkup) return;
-
-  firewallMarkup.innerHTML = `
+    firewallMarkup.innerHTML = `
     <h2>You've entered a Firewall</h2>
-    <div id="roomHeader"></div>
     <p>It is your mission to break it!</p>
+    <div id="roomTimer"></div>
     <div id="cardBoard" class="card-board"></div>
     <button id="back" class="btn-primary">Back</button>
   `;
+  }
 
-  const roomHeader = document.querySelector<HTMLElement>("#roomHeader");
-  if (!roomHeader) return;
-}
+  function renderCards() {
+    const cards = document.querySelector("#cardBoard");
+    if (!cards) return;
 
-function renderCards() {
-  const cardsBoard = document.querySelector("#cardBoard");
-  if (!cardsBoard) return;
+    let cardsMarkup = "";
 
-  let cardsMarkup = "";
-
-  for (let i = 0; i < memoryCards.length; i++) {
-    // 1. Skriv button markup
-    // 2. hämta cards från arrayen
-    // 3. ...
-    cardsMarkup += `
+    for (let i = 0; i < memoryCards.length; i++) {
+      cardsMarkup += `
       <button class="card">
         ${memoryCards[i].flipped ? memoryCards[i].emoji : "🔥"}
       </button>
     `;
-  }
-
-  cardsBoard.innerHTML = cardsMarkup;
-
-  addEventForEachCard();
-}
-
-// ──────────────────────────────────────────────────────────────────────────────────────
-// 🎮 GAME LOGIC
-// ──────────────────────────────────────────────────────────────────────────────────────
-
-/* 
-Simply changing the status of the card from 
-❌ false ➔ ✅ true
-the cards are false to begin with 
-*/
-
-// 1. click on card -> true -> flip
-
-function handleCardClick(index: number) {
-  const clickedCard = memoryCards[index];
-
-  // 🛑 GUARDS
-  if (!clickedCard) return;
-  if (clickedCard.flipped) return;
-
-  // Aktiva öppna kort (inte matchade)
-  const flippedCards = memoryCards.filter(
-    (card) => card.flipped === true && card.matched === false,
-  );
-
-  // avbryt om två redan är öppna
-  if (flippedCards.length >= 2) return;
-
-  // ✅ Ändra status
-  clickedCard.flipped = true;
-
-  console.log(memoryCards[index]);
-
-  renderCards();
-  checkForMatch();
-}
-
-// 2. matchar korten?
-
-function checkForMatch() {
-  // Aktiva öppna kort (inte matchade)
-  const flippedCards = memoryCards.filter(
-    (card) => card.flipped === true && card.matched === false,
-  );
-
-  // kör endast funktionen då två kort är öppna
-  if (flippedCards.length !== 2) return;
-
-  // hämta de två korten öppna korten i den nya arrayen
-  const firstCard = flippedCards[0];
-  const secondCard = flippedCards[1];
-
-  // Change state if match
-  if (firstCard.emoji === secondCard.emoji) {
-    firstCard.matched = true;
-    secondCard.matched = true;
-
-    // All cards are matched -> artefact
-    if (memoryCards.every((card) => card.matched === true)) {
-      handleRoomComplete();
     }
-  } else {
-    setTimeout(function waitForIt() {
-      firstCard.flipped = false;
-      secondCard.flipped = false;
-      renderCards();
-    }, 500);
+
+    cards.innerHTML = cardsMarkup;
+
+    addEventListenerForEachCard();
   }
 
-  console.log("Aktiva öppna:", flippedCards.length);
+  // 🎮 GAME LOGIC
+  // ──────────────────────────────────────────────────────────────────────────────────────
 
-  // No match -> flip back
-}
+  function handleCardClick(index: number) {
+    const clickedCard = memoryCards[index];
+    if (!clickedCard) return;
 
-function handleRoomComplete() {
-  stopRoomTimer();
-  /*const timeLeft = getTimeLeft();
-  player.roomTimes*/
+    if (clickedCard.flipped) return;
 
-  const playerItem = localStorage.getItem("player");
-  if (!playerItem) return;
+    const flippedCards = memoryCards.filter(
+      (card) => card.flipped === true && card.matched === false,
+    );
 
-  const player = JSON.parse(playerItem);
+    if (flippedCards.length >= 2) return;
 
-  // Om artefakt INTE finns..
-  if (!player.artifacts.includes("G")) {
-    // lägg till den
-    player.artifacts.push("G");
-    localStorage.setItem("player", JSON.stringify(player));
-    showPlayerStats();
+    clickedCard.flipped = true;
+
+    console.log(memoryCards[index]);
+
+    renderCards();
+    checkForMatch();
   }
-}
 
-// 4. om ja -> gör ingenting
-// 5. om nej -> vänd tillbaka
+  function checkForMatch() {
+    const flippedCards = memoryCards.filter(
+      (card) => card.flipped === true && card.matched === false,
+    );
 
-// ──────────────────────────────────────────────────────────────────────────────────────
-// 🎧 Hevent-lyssnare
-// ──────────────────────────────────────────────────────────────────────────────────────
+    if (flippedCards.length !== 2) return;
 
-function addEventForEachCard() {
-  // 1. Hämta element som ska ha event-lyssnare
-  const cards = document.querySelectorAll<HTMLButtonElement>(".card");
+    const firstCard = flippedCards[0];
+    const secondCard = flippedCards[1];
 
-  // 2. Sätt event-lyssnare på varje element,
-  // 3. koppla funktion som ska triggas vid klick,
-  // 4. vid klick - kom ihåg vilket kort som klickades
-  cards.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      handleCardClick(index);
+    if (firstCard.emoji === secondCard.emoji) {
+      firstCard.matched = true;
+      secondCard.matched = true;
+    } else {
+      setTimeout(function waitForIt() {
+        firstCard.flipped = false;
+        secondCard.flipped = false;
+        renderCards();
+      }, 500);
+    }
+  }
+
+  // 🎧 event-listeners
+  // ──────────────────────────────────────────────────────────────────────────────────────
+
+  function addEventListenerForEachCard() {
+    const cards = document.querySelectorAll<HTMLButtonElement>(".card");
+
+    cards.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        handleCardClick(index);
+      });
     });
-  });
-}
+  }
 
-function backButtonSetup(onBack: () => void) {
-  // 1. Hämta element
-  const backButton = document.querySelector("#back");
+  // receive a callback passed from loadRoomOne
+  function addEventListenerForBackButton(onBack: () => void) {
+    const backButton = document.querySelector("#back");
 
-  // 2. Sätt event-lyssnare på elementet
-  // 3. koppla indirekt en funktion som ska köras vid klick
-  backButton?.addEventListener("click", onBack);
+    // attach the callback to run on click
+    backButton?.addEventListener("click", onBack);
+  }
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────────────────
+
+  renderRoom();
+  renderCards();
+  addEventListenerForBackButton(onBack);
 }
