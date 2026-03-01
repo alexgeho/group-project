9a3dd150ebab1be2202da606e9bd7f8678e5647c
Mojtaba <mojtaba.mobasheri@hotmail.com>
2026-03-01 08:04:05 +0100

    feat:Add high score display feature and related styles
    - Implemented high score display functionality
    `_highscoreDisplay.scss` for styling the high score section.

---
+++ src/highscoreDisplay.ts
@@ -0,0 +1,34 @@
+import { getHighscoreList } from './highscore.ts';
+
+export function showHighscore(elementName: string): void {
+  const element = document.querySelector(`.${elementName}`)
+  if (element) {
+    const highscoreList = getHighscoreList();
+    let html = `  <section class="high-score">
+      <h2 id="highscore-title">High Score</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank: </th>
+            <th>Player: </th>
+            <th>Score: </th>
+          </tr>
+        </thead>
+        <tbody id="high-score-table-body">`;
+
+    for (let i = 0; i < highscoreList.length; i++) {
+      html += `
+          <tr>
+            <td>${[i+1]}</td>
+            <td>${highscoreList[i].name}</td>
+            <td>${highscoreList[i].score}</td>
+          </tr>`;
+    }
+
+    html += `
+        </tbody>
+      </table>
+    </section>`;
+    element.innerHTML = html;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { loginUser } from "./login.ts";
 import { logout } from "./logout";
 import { fetchRooms } from "./fetchRooms";
 import { checkLoggedInPlayer } from "./checkLoggedInPlayer";
+import { showHighscore } from "./highscoreDisplay.ts";
 
 await fetchRooms();
+showHighscore('high-score-start-page');
 const sections = document.querySelectorAll("main > section");
 sections.forEach((section) => section.classList.add("hidden"));
 document.querySelector("#logout-btn")?.classList.add("hidden");
