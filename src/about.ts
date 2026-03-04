94a1387da561fa170f20a2189ae32497c123238f
Angelie <angelie.angman@gmail.com>
2026-03-04 15:44:01 +0100

    feat(about): add about-page navigation

--- src/about.ts
+++ src/about.ts
@@ -1,4 +1,14 @@
 export function loadAboutPage() {
+  console.log("Om sidan laddas!");
   const aboutSection = document.getElementById("about");
   if (!aboutSection) return;
+
+  const sections = document.querySelectorAll("main > section");
+  sections.forEach((sections) => sections.classList.add("hidden"));
+
+  aboutSection.classList.remove("hidden");
+
+  aboutSection.innerHTML = `
+  <h2>About the Maruders Escape Room</h2>
+  <p></p>`;
 }
